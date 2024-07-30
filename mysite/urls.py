b09ee476066eb70d3bdd0dec4fc59f9da095547c
celee <show3070@gmail.com>
2024-07-30 20:54:22 +0900

    #18. accounts 앱 추가

--- mysite/urls.py
+++ mysite/urls.py
@@ -21,7 +21,7 @@ from django.urls import path, include
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path("", include("accounts.urls")),
+    path("accounts/", include("accounts.urls")),
     path("", include("core.urls")),
 ]
 
