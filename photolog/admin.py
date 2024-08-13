083738aa7da856e5044edfb13ea12ac14dc56da9
celee <show3070@gmail.com>
2024-08-13 21:51:50 +0900

    #31. Note , Photo 모델 정의
    모델 정의 및 업로드 파일명을 uuid 로 일관되게 정의하기

--- photolog/admin.py
+++ photolog/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Photo, Note
+
+
+@admin.register(Note)
+class NoteAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(Photo)
+class PhotoAdmin(admin.ModelAdmin):
+    pass
